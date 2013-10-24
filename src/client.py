fd3b4ae8a6c6c182fc1c6868d929f63e5506be17
Marius Cobzarenco <marius.cobzarenco@gmail.com>
2013-10-24 14:59:42 +0100

    Updated test client

--- src/client.py
+++ src/client.py
@@ -9,9 +9,19 @@ import service_rpcz
 app = rpcz.Application()
 stub = service_rpcz.SourceManager_Stub(app.create_rpc_channel("tcp://127.0.0.1:5555"))
 
-row = source_pb2.SourceRow()
-f = row.features.add()
-f.real = 3.14159 * 2
+req = service_pb2.Request()
+req.source_id = "abcdef"
 
-resp = stub.put_row(row, deadline_ms=1000)
-print('Received response, status=%d' % resp.status)
+row = req.rows.add()
+row.reals.append(3.14159 * 2)
+row.reals.append(-1.0)
+row.categoricals.append("expensive")
+
+row = req.rows.add()
+row.reals.append(2.5)
+row.reals.append(-10.4)
+row.categoricals.append("cheap")
+
+for i in range(10):
+    resp = stub.push_rows(req, deadline_ms=1000)
+    print('Received response, status=%d' % resp.status)
