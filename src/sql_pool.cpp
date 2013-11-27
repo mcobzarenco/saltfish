f992855d79e4558a3d89ef2583626de8d5c56a8f
Marius Cobzarenco <marius.cobzarenco@gmail.com>
2013-11-27 20:54:48 +0000

    SALTFISH added mysql++ connection pool

---
+++ src/sql_pool.cpp
@@ -0,0 +1,48 @@
+#include "sql_pool.hpp"
+
+#include <glog/logging.h>
+
+
+namespace reinferio {
+namespace saltfish {
+namespace sql {
+
+using namespace std;
+
+ConnectionPool::ConnectionPool(const string& db, const string& server,
+                               const string& user, const string& password) :
+    db_(db), server_(server), user_(user), password_(password) {
+}
+
+// The destructor.  We _must_ call ConnectionPool::clear() here,
+// because our superclass can't do it for us.
+ConnectionPool::~ConnectionPool() {
+  clear();
+}
+
+mysqlpp::Connection* ConnectionPool::create() {
+  // Create connection using the parameters we were passed upon
+  // creation.  This could be something much more complex, but for
+  // the purposes of the example, this suffices.
+  LOG(INFO) << "create()" << endl;
+  return new mysqlpp::Connection(db_.c_str(), server_.c_str(),
+                                 user_.c_str(), password_.c_str());
+}
+
+void ConnectionPool::destroy(mysqlpp::Connection* conn) {
+  // Our superclass can't know how we created the Connection, so
+  // it delegates destruction to us, to be safe.
+  LOG(INFO) << "destroy()" << endl;
+  delete conn;
+}
+
+unsigned int ConnectionPool::max_idle_time() {
+  // Set our idle time at an example-friendly 3 seconds.  A real
+  // pool would return some fraction of the server's connection
+  // idle timeout instead.
+  return 3;
+}
+
+}  // namespace sql
+}  // namespace saltfish
+}  // namespace reinferio
---
+++ src/sql_pool.hpp
@@ -0,0 +1,41 @@
+#ifndef REINFERIO_SALTFISH_SQL_POOL_HPP
+#define REINFERIO_SALTFISH_SQL_POOL_HPP
+
+#define MYSQLPP_MYSQL_HEADERS_BURIED
+#include <mysql++/mysql++.h>
+
+#include <string>
+
+
+namespace reinferio {
+namespace saltfish {
+namespace sql {
+
+// TODO: Implement our own connection pool
+// Do not use the abstract base class as it does old style C++ evil things
+class ConnectionPool : public mysqlpp::ConnectionPool {
+ public:
+  ConnectionPool(const std::string& db, const std::string& server,
+                 const std::string& user, const std::string& password);
+  ~ConnectionPool();
+
+  ConnectionPool(const ConnectionPool&) = delete;
+  ConnectionPool& operator=(const ConnectionPool&) = delete;
+
+ protected:
+  mysqlpp::Connection* create() override;
+  void destroy(mysqlpp::Connection* conn) override;
+  unsigned int max_idle_time() override;
+
+ private:
+  const std::string db_;
+  const std::string server_;
+  const std::string user_;
+  const std::string password_;
+};
+
+}  // namespace sql
+}  // namespace saltfish
+}  // namespace reinferio
+
+#endif  // REINFERIO_SALTFISH_SQL_POOL_HPP
