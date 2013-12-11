322d3079c06989d42b0551ad562bc6e2dde26e02
Marius Cobzarenco <marius.cobzarenco@gmail.com>
2013-12-11 17:46:34 +0000

    SALTFISH cheekily added my dir-locals to git

---
+++ src/.dir-locals.el
@@ -0,0 +1,6 @@
+;;; Directory Local Variables
+;;; See Info node `(emacs) Directory Variables' for more information.
+
+((nil .
+  ((flycheck-clang-include-path . ("../build/src"))
+   (flycheck-clang-language-standard . "c++11"))))
---
+++ test/.dir-locals.el
@@ -0,0 +1,6 @@
+;;; Directory Local Variables
+;;; See Info node `(emacs) Directory Variables' for more information.
+
+((nil .
+      ((flycheck-clang-include-path . ("../build/src" "../src" "../build/test/GTest-prefix/src/GTest/include" "../build/test/GMock-prefix/src/GMock/include"))
+       (flycheck-clang-language-standard . "c++11"))))
