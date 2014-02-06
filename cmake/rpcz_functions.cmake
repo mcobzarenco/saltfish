1c0195bb61249be2dadb6239077484afccb27402
Marius Cobzarenco <marius@reinfer.io>
2014-02-06 14:22:14 +0000

    changed rpcz cmake utils to be location agnostic

---
+++ cmake/rpcz_functions.cmake
@@ -0,0 +1,20 @@
+# execute_process(COMMAND which protoc-gen-cpp_rpcz OUTPUT_VARIABLE RPCZ_PLUGIN_CPP_BIN)
+# execute_process(COMMAND which protoc-gen-py_rpcz OUTPUT_VARIABLE RPCZ_PLUGIN_PY_BIN)
+
+function(PROTOBUF_GENERATE_RPCZ SRCS HDRS)
+  PROTOBUF_GENERATE_MULTI(PLUGIN "cpp_rpcz" PROTOS ${ARGN}
+                          OUTPUT_STRUCT "_SRCS:.rpcz.cc;_HDRS:.rpcz.h"
+                          # FLAGS "--plugin=protoc-gen-cpp_rpcz=${RPCZ_PLUGIN_CPP_BIN}"
+                          DEPENDS ${PLUGIN_BIN})
+  set(${SRCS} ${_SRCS} PARENT_SCOPE)
+  set(${HDRS} ${_HDRS} PARENT_SCOPE)
+endfunction()
+
+function(PROTOBUF_GENERATE_PYTHON_RPCZ SRCS)
+  set(PLUGIN_BIN ${RPCZ_PLUGIN_ROOT}/python/protoc-gen-python_rpcz)
+  PROTOBUF_GENERATE_MULTI(PLUGIN "python_rpcz" PROTOS ${ARGN}
+                          OUTPUT_STRUCT "_SRCS:_rpcz.py"
+                          # FLAGS "--plugin=protoc-gen-python_rpcz=${RPCZ_PLUGIN_PY_BIN}"
+                          DEPENDS ${PLUGIN_BIN})
+  set(${SRCS} ${_SRCS} PARENT_SCOPE)
+endfunction()
