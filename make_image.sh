3c54ae5c73f78d6b543713274bca2320211eb0ef
Marius Cobzarenco <marius@reinfer.io>
2014-04-01 19:49:01 +0100

    added script for building the Docker container

---
+++ make_image.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e -v
+
+REPO=saltfish
+REGISTRY=registry.reinfer.io
+
+COMMIT_SHA=$(git log -n 1 --pretty=format:"%H" | cut -c-12)
+IMAGE_SHA=$REGISTRY/$REPO:$COMMIT_SHA
+IMAGE_LATEST=$REGISTRY/$REPO:latest
+
+sudo docker build --rm=true --no-cache=true -t $IMAGE_SHA .
+sudo docker tag $IMAGE_SHA $IMAGE_LATEST
+
+sudo docker push $REGISTRY/$REPO
