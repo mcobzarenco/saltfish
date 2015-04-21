49a39be7221dd07369667bfaf430960ac893bd60
Marius Cobzarenco <marius@reinfer.io>
2015-04-21 15:28:08 +0100

    Tried saving the intermediary images.

--- make_image.sh
+++ make_image.sh
@@ -8,7 +8,7 @@ COMMIT_SHA=$(git log -n 1 --pretty=format:"%H" | cut -c-12)
 IMAGE_SHA=$REGISTRY/$REPO:$COMMIT_SHA
 IMAGE_LATEST=$REGISTRY/$REPO:latest
 
-sudo docker build --rm=true --no-cache=true -t $IMAGE_SHA .
+sudo docker build -t $IMAGE_SHA .
 sudo docker tag -f $IMAGE_SHA $IMAGE_LATEST
 
 sudo docker push $REGISTRY/$REPO
